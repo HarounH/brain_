Fixing sign bug in main function

conv/main.py
@@ -227,7 +227,7 @@ def train(args, train_datasets, train_loaders, test_datasets, test_loaders, mode
                 disc_optimizer.zero_grad()
                 rf, spred, tpred, cpred = model.discriminator(x, predict_s=r_labels, svec=svec, tvec=tvec)
                 # pdb.set_trace()
-                L_gan = torch.log(rf).mean()  # F.binary_cross_entropy(rf.view(-1, 1), r_labels)
+                L_gan = -torch.log(rf).mean()  # F.binary_cross_entropy(rf.view(-1, 1), r_labels)
                 L_clf_s = F.cross_entropy(spred, svec)
                 L_clf_t = F.cross_entropy(tpred, tvec)
                 L_clf_c = F.cross_entropy(cpred, cvec)
@@ -254,7 +254,7 @@ def train(args, train_datasets, train_loaders, test_datasets, test_loaders, mode
                 Gz = model.generator(z, svec, tvec, cvec)
                 rf, spred, tpred, cpred = model.discriminator(x)  # , predict_s=r_labels, svec=svec, tvec=tvec)
                 # pdb.set_trace()
-                L_gan = torch.log(1 - rf).mean()  # F.binary_cross_entropy(rf.view(-1, 1), f_labels)
+                L_gan = -torch.log(1 - rf).mean()  # F.binary_cross_entropy(rf.view(-1, 1), f_labels)
                 # L_clf_s = F.cross_entropy(spred, svec)
                 # L_clf_t = F.cross_entropy(tpred, tvec)
                 # L_clf_c = F.cross_entropy(cpred, cvec)
@@ -278,6 +278,7 @@ def train(args, train_datasets, train_loaders, test_datasets, test_loaders, mode
                 for pn, p in model.discriminator.named_parameters():
                     if p.requires_grad:
                         losses["norm/disc/{}".format(pn)] = p.detach().norm()
+                        losses["norm/disc/grad{}".format(pn)] = p.grad.detach().norm()
                 disc_optimizer.step()
                 gradient_penalty_time += time.time() - grad_start
 
@@ -315,6 +316,7 @@ def train(args, train_datasets, train_loaders, test_datasets, test_loaders, mode
             for pn, p in model.generator.named_parameters():
                 if p.requires_grad:
                     losses["norm/gen/{}".format(pn)] = p.detach().norm()
+                    losses["norm/gen/grad/{}".format(pn)] = p.grad.detach().norm()
             gen_optimizer.step()
 
             backward_prop_time += time.time() - temp_time

conv/modules/generators.py
@@ -47,29 +47,29 @@ class GeneratorHierarchical0(nn.Module):
             nn.Dropout(p=dropout_rate),
         )
 
-        self.study_embedding = weight_norm(nn.Embedding(len(meta['s2i']), content_channels))
-        self.task_embedding = weight_norm(nn.Embedding(len(meta['t2i']), content_channels))
-        self.contrast_embedding = weight_norm(nn.Embedding(len(meta['c2i']), content_channels))
+        self.study_embedding = nn.Embedding(len(meta['s2i']), content_channels)
+        self.task_embedding = nn.Embedding(len(meta['t2i']), content_channels)
+        self.contrast_embedding = nn.Embedding(len(meta['c2i']), content_channels)
 
         self.fcs = nn.ModuleList([
             nn.Sequential(
-                weight_norm(nn.Linear(content_channels, content_channels)),
+                nn.Linear(content_channels, content_channels),
                 nn.Dropout(dropout_rate),
             ),
             nn.Sequential(
-                weight_norm(nn.Linear(2 * content_channels, content_channels)),
+                nn.Linear(2 * content_channels, content_channels),
                 nn.Dropout(dropout_rate),
             ),
             nn.Sequential(
-                weight_norm(nn.Linear(3 * content_channels, content_channels)),
+                nn.Linear(3 * content_channels, content_channels),
                 nn.Dropout(dropout_rate),
             ),
             nn.Sequential(
-                weight_norm(nn.Linear(3 * content_channels, content_channels)),
+                nn.Linear(3 * content_channels, content_channels),
                 nn.Dropout(dropout_rate),
             ),
             nn.Sequential(
-                weight_norm(nn.Linear(3 * content_channels, content_channels)),
+                nn.Linear(3 * content_channels, content_channels),
                 nn.Dropout(dropout_rate),
             ),
         ])

utils/utils.py
@@ -12,7 +12,7 @@ from nilearn import plotting
 import nibabel
 from nilearn.input_data import NiftiMasker
 import nilearn.masking as masking
-from nilearn.image import load_img, resample_img
+from nilearn.image import load_img, resample_img, math_img
 import torch
 from torch.utils.data import SubsetRandomSampler
 from collections import defaultdict
@@ -59,14 +59,17 @@ def save_images(tensor_list, title_list, filename, mu_=0.0, std_=1.0, figsize=(1
     for idx, tensor in enumerate(tensor_list):
         ax = plt.subplot(nrows, ncols, indexes[idx] if indexes else idx)
         plotting.plot_stat_map(
-            nibabel.Nifti1Image(
-                mu_ + std_ * tensor.detach().cpu().numpy().squeeze(), constants.brain_mask.affine
+            math_img("img1 * img2",
+                img1=nibabel.Nifti1Image(
+                    mu_ + std_ * tensor.detach().cpu().numpy().squeeze(), constants.brain_mask.affine
+                ),
+                img2=constants.brain_mask
             ),
             axes=ax,
             title=title_list[idx],
             cut_coords=cut_coords,
             # bg_img=downsampled_template,
-            threshold='auto',
+            threshold=False,
             black_bg=False,
         )
     if show_instead_of_save: