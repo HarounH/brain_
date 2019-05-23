dd97ef74389b8b04f6712c26137caf7dfb8d488b
Haroun Habeebe <haroun7@gmail.com>
2019-05-23 05:55:07 -0500

    Pre-neurips

--- gcn/modules/fgl_clf.py
+++ gcn/modules/fgl_clf.py
@@ -312,6 +312,366 @@ class SmallerClassifier(nn.Module):
         return self.fc(cur_z.view(N, -1))
 
 
+class C1SmallerClassifier(nn.Module):
+    def __init__(self, args, loadable_state_dict=None, z_size=32, dropout_rate=0.5):
+        super().__init__()
+        self.args = args
+        meta = self.args.meta
+        wtree = args.wtree
+        self.z_size = z_size
+        in_features = constants.original_masked_nnz
+
+        self.node_sizes = [in_features, 1024]#, 256, 32]
+        self.channel_sizes = [1, 1]#, 64, 128]  # That mapping should be fairly fast
+        # self.channel_sizes = [1, 2, 4, 8]  # That mapping should be fairly fast
+        adj_list = []
+
+        cur_level = wtree.get_leaves()
+        for next_count in self.node_sizes[1:]:
+            cur_level, _, adj = wtree.get_level_and_adjacency(next_count, cur_level)
+            # cur_level, _, adj = ward_tree.go_up_to_reduce(cur_level, next_count)
+            adj_list.append(adj)
+        # adj_list contains adj list from ~200k->...->128
+        # we need to transpose each one and them reverse the list
+        self.n_layers = len(self.channel_sizes) - 1
+        OP_ORDER = args.op_order  # "132"
+        REDUCTION = args.reduction
+        OPTIMIZATION = args.optimization
+        self.downsample0 = fgl.make_weight_normed_FGL(
+            int(self.channel_sizes[0]),
+            int(self.node_sizes[0]),
+            int(self.channel_sizes[1]),
+            int(self.node_sizes[1]),
+            adj_list[0],
+            op_order=OP_ORDER,
+            reduction=REDUCTION,
+            optimization=OPTIMIZATION,
+        )
+        self.activation0 = nn.Sequential()
+        # self.downsample1 = fgl.make_weight_normed_FGL(
+        #     int(self.channel_sizes[1]),
+        #     int(self.node_sizes[1]),
+        #     int(self.channel_sizes[2]),
+        #     int(self.node_sizes[2]),
+        #     adj_list[1],
+        #     op_order=OP_ORDER,
+        #     reduction=REDUCTION,
+        #     optimization=OPTIMIZATION,
+        # )
+        # self.activation1 = nn.Sequential()
+        # self.downsample2 = fgl.make_weight_normed_FGL(
+        #     int(self.channel_sizes[2]),
+        #     int(self.node_sizes[2]),
+        #     int(self.channel_sizes[3]),
+        #     int(self.node_sizes[3]),
+        #     adj_list[2],
+        #     op_order=OP_ORDER,
+        #     reduction=REDUCTION,
+        #     optimization=OPTIMIZATION,
+        # )
+        # self.activation2 = nn.Sequential()
+        self.fc = nn.Sequential(
+            weight_norm(nn.Linear(self.node_sizes[-1] * self.channel_sizes[-1], len(meta['c2i']))),
+        )
+
+    def forward(self, x):
+        # x: N, constants.masked_nnz
+        N = x.shape[0]
+        cur_z = x.unsqueeze(1)  # N, 1, constants.masked_nnz
+        for i in range(self.n_layers):
+            cur_z = getattr(self, 'downsample{}'.format(i))(cur_z)
+            cur_z = getattr(self, 'activation{}'.format(i))(cur_z)
+        return self.fc(cur_z.view(N, -1))
+
+
+class C2SmallerClassifier(nn.Module):
+    def __init__(self, args, loadable_state_dict=None, z_size=32, dropout_rate=0.5):
+        super().__init__()
+        self.args = args
+        meta = self.args.meta
+        wtree = args.wtree
+        self.z_size = z_size
+        in_features = constants.original_masked_nnz
+
+        self.node_sizes = [in_features, 1024]#, 256, 32]
+        self.channel_sizes = [1, 2]#, 64, 128]  # That mapping should be fairly fast
+        # self.channel_sizes = [1, 2, 4, 8]  # That mapping should be fairly fast
+        adj_list = []
+
+        cur_level = wtree.get_leaves()
+        for next_count in self.node_sizes[1:]:
+            cur_level, _, adj = wtree.get_level_and_adjacency(next_count, cur_level)
+            # cur_level, _, adj = ward_tree.go_up_to_reduce(cur_level, next_count)
+            adj_list.append(adj)
+        # adj_list contains adj list from ~200k->...->128
+        # we need to transpose each one and them reverse the list
+        self.n_layers = len(self.channel_sizes) - 1
+        OP_ORDER = args.op_order  # "132"
+        REDUCTION = args.reduction
+        OPTIMIZATION = args.optimization
+        self.downsample0 = fgl.make_weight_normed_FGL(
+            int(self.channel_sizes[0]),
+            int(self.node_sizes[0]),
+            int(self.channel_sizes[1]),
+            int(self.node_sizes[1]),
+            adj_list[0],
+            op_order=OP_ORDER,
+            reduction=REDUCTION,
+            optimization=OPTIMIZATION,
+        )
+        self.activation0 = nn.Sequential()
+        # self.downsample1 = fgl.make_weight_normed_FGL(
+        #     int(self.channel_sizes[1]),
+        #     int(self.node_sizes[1]),
+        #     int(self.channel_sizes[2]),
+        #     int(self.node_sizes[2]),
+        #     adj_list[1],
+        #     op_order=OP_ORDER,
+        #     reduction=REDUCTION,
+        #     optimization=OPTIMIZATION,
+        # )
+        # self.activation1 = nn.Sequential()
+        # self.downsample2 = fgl.make_weight_normed_FGL(
+        #     int(self.channel_sizes[2]),
+        #     int(self.node_sizes[2]),
+        #     int(self.channel_sizes[3]),
+        #     int(self.node_sizes[3]),
+        #     adj_list[2],
+        #     op_order=OP_ORDER,
+        #     reduction=REDUCTION,
+        #     optimization=OPTIMIZATION,
+        # )
+        # self.activation2 = nn.Sequential()
+        self.fc = nn.Sequential(
+            weight_norm(nn.Linear(self.node_sizes[-1] * self.channel_sizes[-1], len(meta['c2i']))),
+        )
+
+    def forward(self, x):
+        # x: N, constants.masked_nnz
+        N = x.shape[0]
+        cur_z = x.unsqueeze(1)  # N, 1, constants.masked_nnz
+        for i in range(self.n_layers):
+            cur_z = getattr(self, 'downsample{}'.format(i))(cur_z)
+            cur_z = getattr(self, 'activation{}'.format(i))(cur_z)
+        return self.fc(cur_z.view(N, -1))
+
+
+class C4SmallerClassifier(nn.Module):
+    def __init__(self, args, loadable_state_dict=None, z_size=32, dropout_rate=0.5):
+        super().__init__()
+        self.args = args
+        meta = self.args.meta
+        wtree = args.wtree
+        self.z_size = z_size
+        in_features = constants.original_masked_nnz
+
+        self.node_sizes = [in_features, 1024]#, 256, 32]
+        self.channel_sizes = [1, 4]#, 64, 128]  # That mapping should be fairly fast
+        # self.channel_sizes = [1, 2, 4, 8]  # That mapping should be fairly fast
+        adj_list = []
+
+        cur_level = wtree.get_leaves()
+        for next_count in self.node_sizes[1:]:
+            cur_level, _, adj = wtree.get_level_and_adjacency(next_count, cur_level)
+            # cur_level, _, adj = ward_tree.go_up_to_reduce(cur_level, next_count)
+            adj_list.append(adj)
+        # adj_list contains adj list from ~200k->...->128
+        # we need to transpose each one and them reverse the list
+        self.n_layers = len(self.channel_sizes) - 1
+        OP_ORDER = args.op_order  # "132"
+        REDUCTION = args.reduction
+        OPTIMIZATION = args.optimization
+        self.downsample0 = fgl.make_weight_normed_FGL(
+            int(self.channel_sizes[0]),
+            int(self.node_sizes[0]),
+            int(self.channel_sizes[1]),
+            int(self.node_sizes[1]),
+            adj_list[0],
+            op_order=OP_ORDER,
+            reduction=REDUCTION,
+            optimization=OPTIMIZATION,
+        )
+        self.activation0 = nn.Sequential()
+        # self.downsample1 = fgl.make_weight_normed_FGL(
+        #     int(self.channel_sizes[1]),
+        #     int(self.node_sizes[1]),
+        #     int(self.channel_sizes[2]),
+        #     int(self.node_sizes[2]),
+        #     adj_list[1],
+        #     op_order=OP_ORDER,
+        #     reduction=REDUCTION,
+        #     optimization=OPTIMIZATION,
+        # )
+        # self.activation1 = nn.Sequential()
+        # self.downsample2 = fgl.make_weight_normed_FGL(
+        #     int(self.channel_sizes[2]),
+        #     int(self.node_sizes[2]),
+        #     int(self.channel_sizes[3]),
+        #     int(self.node_sizes[3]),
+        #     adj_list[2],
+        #     op_order=OP_ORDER,
+        #     reduction=REDUCTION,
+        #     optimization=OPTIMIZATION,
+        # )
+        # self.activation2 = nn.Sequential()
+        self.fc = nn.Sequential(
+            weight_norm(nn.Linear(self.node_sizes[-1] * self.channel_sizes[-1], len(meta['c2i']))),
+        )
+
+    def forward(self, x):
+        # x: N, constants.masked_nnz
+        N = x.shape[0]
+        cur_z = x.unsqueeze(1)  # N, 1, constants.masked_nnz
+        for i in range(self.n_layers):
+            cur_z = getattr(self, 'downsample{}'.format(i))(cur_z)
+            cur_z = getattr(self, 'activation{}'.format(i))(cur_z)
+        return self.fc(cur_z.view(N, -1))
+
+
+class C8SmallerClassifier(nn.Module):
+    def __init__(self, args, loadable_state_dict=None, z_size=32, dropout_rate=0.5):
+        super().__init__()
+        self.args = args
+        meta = self.args.meta
+        wtree = args.wtree
+        self.z_size = z_size
+        in_features = constants.original_masked_nnz
+
+        self.node_sizes = [in_features, 1024]#, 256, 32]
+        self.channel_sizes = [1, 8]#, 64, 128]  # That mapping should be fairly fast
+        # self.channel_sizes = [1, 2, 4, 8]  # That mapping should be fairly fast
+        adj_list = []
+
+        cur_level = wtree.get_leaves()
+        for next_count in self.node_sizes[1:]:
+            cur_level, _, adj = wtree.get_level_and_adjacency(next_count, cur_level)
+            # cur_level, _, adj = ward_tree.go_up_to_reduce(cur_level, next_count)
+            adj_list.append(adj)
+        # adj_list contains adj list from ~200k->...->128
+        # we need to transpose each one and them reverse the list
+        self.n_layers = len(self.channel_sizes) - 1
+        OP_ORDER = args.op_order  # "132"
+        REDUCTION = args.reduction
+        OPTIMIZATION = args.optimization
+        self.downsample0 = fgl.make_weight_normed_FGL(
+            int(self.channel_sizes[0]),
+            int(self.node_sizes[0]),
+            int(self.channel_sizes[1]),
+            int(self.node_sizes[1]),
+            adj_list[0],
+            op_order=OP_ORDER,
+            reduction=REDUCTION,
+            optimization=OPTIMIZATION,
+        )
+        self.activation0 = nn.Sequential()
+        # self.downsample1 = fgl.make_weight_normed_FGL(
+        #     int(self.channel_sizes[1]),
+        #     int(self.node_sizes[1]),
+        #     int(self.channel_sizes[2]),
+        #     int(self.node_sizes[2]),
+        #     adj_list[1],
+        #     op_order=OP_ORDER,
+        #     reduction=REDUCTION,
+        #     optimization=OPTIMIZATION,
+        # )
+        # self.activation1 = nn.Sequential()
+        # self.downsample2 = fgl.make_weight_normed_FGL(
+        #     int(self.channel_sizes[2]),
+        #     int(self.node_sizes[2]),
+        #     int(self.channel_sizes[3]),
+        #     int(self.node_sizes[3]),
+        #     adj_list[2],
+        #     op_order=OP_ORDER,
+        #     reduction=REDUCTION,
+        #     optimization=OPTIMIZATION,
+        # )
+        # self.activation2 = nn.Sequential()
+        self.fc = nn.Sequential(
+            weight_norm(nn.Linear(self.node_sizes[-1] * self.channel_sizes[-1], len(meta['c2i']))),
+        )
+
+    def forward(self, x):
+        # x: N, constants.masked_nnz
+        N = x.shape[0]
+        cur_z = x.unsqueeze(1)  # N, 1, constants.masked_nnz
+        for i in range(self.n_layers):
+            cur_z = getattr(self, 'downsample{}'.format(i))(cur_z)
+            cur_z = getattr(self, 'activation{}'.format(i))(cur_z)
+        return self.fc(cur_z.view(N, -1))
+
+
+class C16SmallerClassifier(nn.Module):
+    def __init__(self, args, loadable_state_dict=None, z_size=32, dropout_rate=0.5):
+        super().__init__()
+        self.args = args
+        meta = self.args.meta
+        wtree = args.wtree
+        self.z_size = z_size
+        in_features = constants.original_masked_nnz
+
+        self.node_sizes = [in_features, 1024]#, 256, 32]
+        self.channel_sizes = [1, 16]#, 64, 128]  # That mapping should be fairly fast
+        # self.channel_sizes = [1, 2, 4, 8]  # That mapping should be fairly fast
+        adj_list = []
+
+        cur_level = wtree.get_leaves()
+        for next_count in self.node_sizes[1:]:
+            cur_level, _, adj = wtree.get_level_and_adjacency(next_count, cur_level)
+            # cur_level, _, adj = ward_tree.go_up_to_reduce(cur_level, next_count)
+            adj_list.append(adj)
+        # adj_list contains adj list from ~200k->...->128
+        # we need to transpose each one and them reverse the list
+        self.n_layers = len(self.channel_sizes) - 1
+        OP_ORDER = args.op_order  # "132"
+        REDUCTION = args.reduction
+        OPTIMIZATION = args.optimization
+        self.downsample0 = fgl.make_weight_normed_FGL(
+            int(self.channel_sizes[0]),
+            int(self.node_sizes[0]),
+            int(self.channel_sizes[1]),
+            int(self.node_sizes[1]),
+            adj_list[0],
+            op_order=OP_ORDER,
+            reduction=REDUCTION,
+            optimization=OPTIMIZATION,
+        )
+        self.activation0 = nn.Sequential()
+        # self.downsample1 = fgl.make_weight_normed_FGL(
+        #     int(self.channel_sizes[1]),
+        #     int(self.node_sizes[1]),
+        #     int(self.channel_sizes[2]),
+        #     int(self.node_sizes[2]),
+        #     adj_list[1],
+        #     op_order=OP_ORDER,
+        #     reduction=REDUCTION,
+        #     optimization=OPTIMIZATION,
+        # )
+        # self.activation1 = nn.Sequential()
+        # self.downsample2 = fgl.make_weight_normed_FGL(
+        #     int(self.channel_sizes[2]),
+        #     int(self.node_sizes[2]),
+        #     int(self.channel_sizes[3]),
+        #     int(self.node_sizes[3]),
+        #     adj_list[2],
+        #     op_order=OP_ORDER,
+        #     reduction=REDUCTION,
+        #     optimization=OPTIMIZATION,
+        # )
+        # self.activation2 = nn.Sequential()
+        self.fc = nn.Sequential(
+            weight_norm(nn.Linear(self.node_sizes[-1] * self.channel_sizes[-1], len(meta['c2i']))),
+        )
+
+    def forward(self, x):
+        # x: N, constants.masked_nnz
+        N = x.shape[0]
+        cur_z = x.unsqueeze(1)  # N, 1, constants.masked_nnz
+        for i in range(self.n_layers):
+            cur_z = getattr(self, 'downsample{}'.format(i))(cur_z)
+            cur_z = getattr(self, 'activation{}'.format(i))(cur_z)
+        return self.fc(cur_z.view(N, -1))
+
+
 class SmallerClassifier_Residual(nn.Module):
     def __init__(self, args, loadable_state_dict=None, z_size=32, dropout_rate=0.5):
         super().__init__()
--- run_all.sh
+++ run_all.sh
@@ -14,26 +14,56 @@
 # python -m gcn.multi_run brainomics --cuda -dp -mine 10 -maxe 50 -ct redfc -ok 10 -r brainomics0 --not_lazy
 # python -m gcn.multi_run hcp --cuda -dp -mine 10 -maxe 50 -ct redfc -ok 10 -r hcp0
 
+## Channel size ablation
+# python -m gcn.multi_run camcan --cuda -dp -mine 2 -maxe 10 -ct c1smallerfgl_213_sum_tree -ok 4 -r camcan0 --not_lazy
+# python -m gcn.multi_run archi --cuda -dp -mine 2 -maxe 10 -ct c1smallerfgl_213_sum_tree -ok 4 -r archi0 --not_lazy
+# python -m gcn.multi_run la5c --cuda -dp -mine 2 -maxe 10 -ct c1smallerfgl_213_sum_tree -ok 4 -r la5c0 --not_lazy
+# python -m gcn.multi_run brainomics --cuda -dp -mine 2 -maxe 10 -ct c1smallerfgl_213_sum_tree -ok 4 -r brainomics0 --not_lazy
+# python -m gcn.multi_run hcp --cuda -dp -mine 2 -maxe 10 -ct c1smallerfgl_213_sum_tree -ok 4 -r hcp0
+
+# python -m gcn.multi_run camcan --cuda -dp -mine 2 -maxe 10 -ct c2smallerfgl_213_sum_tree -ok 4 -r camcan0 --not_lazy
+# python -m gcn.multi_run archi --cuda -dp -mine 2 -maxe 10 -ct c2smallerfgl_213_sum_tree -ok 4 -r archi0 --not_lazy
+# python -m gcn.multi_run la5c --cuda -dp -mine 2 -maxe 10 -ct c2smallerfgl_213_sum_tree -ok 4 -r la5c0 --not_lazy
+# python -m gcn.multi_run brainomics --cuda -dp -mine 2 -maxe 10 -ct c2smallerfgl_213_sum_tree -ok 4 -r brainomics0 --not_lazy
+# python -m gcn.multi_run hcp --cuda -dp -mine 2 -maxe 10 -ct c2smallerfgl_213_sum_tree -ok 4 -r hcp0
+
+# python -m gcn.multi_run camcan --cuda -dp -mine 2 -maxe 10 -ct c4smallerfgl_213_sum_tree -ok 4 -r camcan0 --not_lazy
+# python -m gcn.multi_run archi --cuda -dp -mine 2 -maxe 10 -ct c4smallerfgl_213_sum_tree -ok 4 -r archi0 --not_lazy
+# python -m gcn.multi_run la5c --cuda -dp -mine 2 -maxe 10 -ct c4smallerfgl_213_sum_tree -ok 4 -r la5c0 --not_lazy
+# python -m gcn.multi_run brainomics --cuda -dp -mine 2 -maxe 10 -ct c4smallerfgl_213_sum_tree -ok 4 -r brainomics0 --not_lazy
+# python -m gcn.multi_run hcp --cuda -dp -mine 2 -maxe 10 -ct c4smallerfgl_213_sum_tree -ok 4 -r hcp0
+
+python -m gcn.multi_run camcan --cuda -dp -mine 2 -maxe 10 -ct c8smallerfgl_213_sum_tree -ok 4 -r camcan0 --not_lazy
+python -m gcn.multi_run archi --cuda -dp -mine 2 -maxe 10 -ct c8smallerfgl_213_sum_tree -ok 4 -r archi0 --not_lazy
+python -m gcn.multi_run la5c --cuda -dp -mine 2 -maxe 10 -ct c8smallerfgl_213_sum_tree -ok 4 -r la5c0 --not_lazy
+python -m gcn.multi_run brainomics --cuda -dp -mine 2 -maxe 10 -ct c8smallerfgl_213_sum_tree -ok 4 -r brainomics0 --not_lazy
+# python -m gcn.multi_run hcp --cuda -dp -mine 2 -maxe 10 -ct c8smallerfgl_213_sum_tree -ok 4 -r hcp0
+
+# python -m gcn.multi_run camcan --cuda -dp -mine 2 -maxe 10 -ct c16smallerfgl_213_sum_tree -ok 4 -r camcan0 --not_lazy
+# python -m gcn.multi_run archi --cuda -dp -mine 2 -maxe 10 -ct c16smallerfgl_213_sum_tree -ok 4 -r archi0 --not_lazy
+# python -m gcn.multi_run la5c --cuda -dp -mine 2 -maxe 10 -ct c16smallerfgl_213_sum_tree -ok 4 -r la5c0 --not_lazy
+# python -m gcn.multi_run brainomics --cuda -dp -mine 2 -maxe 10 -ct c16smallerfgl_213_sum_tree -ok 4 -r brainomics0 --not_lazy
+# python -m gcn.multi_run hcp --cuda -dp -mine 2 -maxe 10 -ct c16smallerfgl_213_sum_tree -ok 4 -r hcp0
 
 # Ablation start
-python -m gcn.multi_run camcan --cuda -dp -mine 10 -maxe 20 -ct residualsmallerfgl_213_sum_tree -ok 4 -r camcan0 --not_lazy
-python -m gcn.multi_run archi --cuda -dp -mine 10 -maxe 20 -ct residualsmallerfgl_213_sum_tree -ok 4 -r archi0 --not_lazy
-python -m gcn.multi_run la5c --cuda -dp -mine 10 -maxe 20 -ct residualsmallerfgl_213_sum_tree -ok 4 -r la5c0 --not_lazy
-python -m gcn.multi_run brainomics --cuda -dp -mine 10 -maxe 20 -ct residualsmallerfgl_213_sum_tree -ok 4 -r brainomics0 --not_lazy
-
-# python -m gcn.multi_run camcan --cuda -dp -mine 10 -maxe 20 -ct 1smallerfgl_213_sum_tree -ok 4 -r camcan0 --not_lazy
-# python -m gcn.multi_run archi --cuda -dp -mine 10 -maxe 20 -ct 1smallerfgl_213_sum_tree -ok 4 -r archi0 --not_lazy
-# python -m gcn.multi_run la5c --cuda -dp -mine 10 -maxe 20 -ct 1smallerfgl_213_sum_tree -ok 4 -r la5c0 --not_lazy
-# python -m gcn.multi_run brainomics --cuda -dp -mine 10 -maxe 20 -ct 1smallerfgl_213_sum_tree -ok 4 -r brainomics0 --not_lazy
+# python -m gcn.multi_run camcan --cuda -dp -mine 10 -maxe 20 -ct residualsmallerfgl_213_sum_tree -ok 10 -r camcan1 --not_lazy
+# python -m gcn.multi_run archi --cuda -dp -mine 10 -maxe 20 -ct residualsmallerfgl_213_sum_tree -ok 10 -r archi1 --not_lazy
+# python -m gcn.multi_run la5c --cuda -dp -mine 10 -maxe 20 -ct residualsmallerfgl_213_sum_tree -ok 10 -r la5c1 --not_lazy
+# python -m gcn.multi_run brainomics --cuda -dp -mine 10 -maxe 20 -ct residualsmallerfgl_213_sum_tree -ok 10 -r brainomics1 --not_lazy
 #
-# python -m gcn.multi_run camcan --cuda -dp -mine 10 -maxe 20 -ct 2smallerfgl_213_sum_tree -ok 4 -r camcan0 --not_lazy
-# python -m gcn.multi_run archi --cuda -dp -mine 10 -maxe 20 -ct 2smallerfgl_213_sum_tree -ok 4 -r archi0 --not_lazy
-# python -m gcn.multi_run la5c --cuda -dp -mine 10 -maxe 20 -ct 2smallerfgl_213_sum_tree -ok 4 -r la5c0 --not_lazy
-# python -m gcn.multi_run brainomics --cuda -dp -mine 10 -maxe 20 -ct 2smallerfgl_213_sum_tree -ok 4 -r brainomics0 --not_lazy
+# python -m gcn.multi_run camcan --cuda -dp -mine 10 -maxe 20 -ct 1smallerfgl_213_sum_tree -ok 10 -r camcan1 --not_lazy
+# python -m gcn.multi_run archi --cuda -dp -mine 10 -maxe 20 -ct 1smallerfgl_213_sum_tree -ok 10 -r archi1 --not_lazy
+# python -m gcn.multi_run la5c --cuda -dp -mine 10 -maxe 20 -ct 1smallerfgl_213_sum_tree -ok 10 -r la5c1 --not_lazy
+# python -m gcn.multi_run brainomics --cuda -dp -mine 10 -maxe 20 -ct 1smallerfgl_213_sum_tree -ok 10 -r brainomics1 --not_lazy
 #
-# python -m gcn.multi_run hcp --cuda -dp -mine 10 -maxe 20 -ct 1smallerfgl_213_sum_tree -ok 4 -r hcp0
-# python -m gcn.multi_run hcp --cuda -dp -mine 10 -maxe 20 -ct 2smallerfgl_213_sum_tree -ok 4 -r hcp0
-python -m gcn.multi_run hcp --cuda -dp -mine 10 -maxe 20 -ct residualsmallerfgl_213_sum_tree -ok 4 -r hcp0
+# python -m gcn.multi_run camcan --cuda -dp -mine 10 -maxe 20 -ct 2smallerfgl_213_sum_tree -ok 10 -r camcan1 --not_lazy
+# python -m gcn.multi_run archi --cuda -dp -mine 10 -maxe 20 -ct 2smallerfgl_213_sum_tree -ok 10 -r archi1 --not_lazy
+# python -m gcn.multi_run la5c --cuda -dp -mine 10 -maxe 20 -ct 2smallerfgl_213_sum_tree -ok 10 -r la5c1 --not_lazy
+# python -m gcn.multi_run brainomics --cuda -dp -mine 10 -maxe 20 -ct 2smallerfgl_213_sum_tree -ok 10 -r brainomics1 --not_lazy
+#
+# python -m gcn.multi_run hcp --cuda -dp -mine 10 -maxe 20 -ct 1smallerfgl_213_sum_tree -ok 10 -r hcp1
+# python -m gcn.multi_run hcp --cuda -dp -mine 10 -maxe 20 -ct 2smallerfgl_213_sum_tree -ok 10 -r hcp1
+# python -m gcn.multi_run hcp --cuda -dp -mine 10 -maxe 20 -ct residualsmallerfgl_213_sum_tree -ok 10 -r hcp1
 # Ablation end
 
 # brainomics
